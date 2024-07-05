5990dd7ee60bde44fea8d58423aea18d34adb053
thisizaro <thisisaro.official@gmail.com>
2024-07-05 20:23:46 +0530

    First commit... just adding the script. Hope I don't forget to drink water now!

---
+++ script.py
@@ -0,0 +1,16 @@
+import time
+from plyer import notification
+
+def remind_to_drink_water(interval_minutes):
+    interval_seconds = interval_minutes * 60
+    while True:
+        time.sleep(interval_seconds)
+        notification.notify(
+            title='Hydration Reminder',
+            message='It\'s time to drink water!',
+            timeout=10
+        )
+
+# Set the interval time in minutes
+interval_minutes = 60  # for example, set to 60 minutes
+remind_to_drink_water(interval_minutes)
