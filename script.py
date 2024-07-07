86aca32465a0f54b2a8ba2906103bdaaf7fbd05b
thisizaro <thisisaro.official@gmail.com>
2024-07-07 21:54:58 +0530

    timer update and bat file added.

--- script.py
+++ script.py
@@ -1,11 +1,24 @@
 import time
 from plyer import notification
 
+def timer(n):
+    print("Timer started!")
+    while n > 0:
+        n = n - 1
+        time.sleep(1)
+        s = str(n)
+        for i in range(0, len(s)):
+            print("\b", end="", flush=True)
+        print(n, end = "", flush=True)
+    
+
 def remind_to_drink_water(interval_minutes):
     interval_seconds = interval_minutes * 60
+    count = 0
     while True:
-        print("Running...")
-        time.sleep(interval_seconds)
+        print("Running...", count)
+        count += 1
+        timer(interval_seconds)
         notification.notify(
             title='Hydration Reminder',
             message='It\'s time to drink water!',
